ae51aac82ad56f1daa803d5d08565eea4077dc2e
DHViet-16 <hoangvietpk16@gmail.com>
2024-03-21 16:55:39 +0700

    file code ngay 21/03

---
+++ uthexercise1/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Demo1403 from './components/demo1403';
+import Demo2103 from './components/demo2103';
+
+function App() {
+  return (
+    <div>
+       {/* <Demo1403 /> */}
+       <Demo2103 />
+    </div>
+  )
+}
+
+export default App;
