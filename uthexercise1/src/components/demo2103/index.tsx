4a0ec6c912e5e59367d3717a5f17d8683862fe3c
DHViet-16 <hoangvietpk16@gmail.com>
2024-03-28 10:14:43 +0700

    file code 28/03

--- uthexercise1/src/components/demo2103/index.tsx
+++ uthexercise1/src/components/demo2103/index.tsx
@@ -1,6 +1,7 @@
 import { Table } from "antd";
-import React from "react";
+import React, { useState } from "react";
 const Demo2103 = () => {
+  const [searchData, setSearchData] = useState<{ key: string; name: string; age: number; address: string; }[]>([]);
   const dataSource = [
     {
       key: '1',
@@ -36,7 +37,14 @@ const Demo2103 = () => {
 
   return (
     <div>
-      <Table columns={columns} dataSource={dataSource} />
+      <select onChange={(e) => {
+        setSearchData(dataSource.filter(x => x.age < Number(e.currentTarget.value) ));
+      }}>
+        <option value="30">Under Age:30</option>
+        <option value="40">Under Age:40</option>
+        <option value="50">Under Age:50</option>
+      </select>
+      <Table columns={columns} dataSource={searchData} />
       <button onClick={() => {
         localStorage.clear();
         window.location.reload();
